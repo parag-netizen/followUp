d73084aad6018de89e05280a2e3c29f9ccf69b27
parag-netizen <parag1498@gmail.com>
2024-12-10 13:42:43 +0530

    Diagram_pane updated

--- frontend/src/Components/SinglePost.jsx
+++ frontend/src/Components/SinglePost.jsx
@@ -31,11 +31,12 @@ const SinglePost = () => {
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id rem deserunt autem numquam veritatis odio quia in sit atque veniam illo corporis, repellendus hic dolores commodi suscipit iste sint architecto.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa saepe doloremque debitis explicabo adipisci molestiae perspiciatis, dignissimos mollitia hic sunt quos tempora. Delectus ipsum earum ipsa eveniet repellat mollitia iste!Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa saepe doloremque debitis explicabo adipisci molestiae perspiciatis, dignissimos mollitia hic sunt quos tempora. Delectus ipsum earum ipsa eveniet repellat mollitia iste!Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa saepe doloremque debitis explicabo adipisci molestiae perspiciatis, dignissimos mollitia hic sunt quos tempora. Delectus ipsum earum ipsa eveniet repellat mollitia iste!Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa saepe doloremque debitis explicabo adipisci molestiae perspiciatis, dignissimos mollitia hic sunt quos tempora. Delectus ipsum earum ipsa eveniet repellat mollitia iste!Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa saepe doloremque debitis explicabo adipisci molestiae perspiciatis, dignissimos mollitia hic sunt quos tempora. Delectus ipsum earum ipsa eveniet repellat mollitia iste!</p>
             </div>
-            <div className='menu'>
-                <Whiteboard >
 
-                </Whiteboard>
-            </div>
+        </div>
+        <div className='pane'>
+            <Whiteboard>
+
+            </Whiteboard>
         </div>
     </>
 }
--- frontend/src/Components/Whiteboard.jsx
+++ frontend/src/Components/Whiteboard.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import '../Components/Whiteboard.css'
 import { ReactFlow } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import { FaExpand } from "react-icons/fa6";
+import { FiMaximize } from "react-icons/fi";
+import { FiMinimize } from "react-icons/fi";
+
 
 const Whiteboard = () => {
+
+    const Diagrampane = useRef(null)
+    const expand1 = useRef(null)
+    const minimize = useRef(null)
+
     const expand_box = () => {
-
+        Diagrampane.current.style.width = '1700px'
+        expand1.current.style.display = 'none';
+        minimize.current.style.display = 'flex';
+        console.log(Diagrampane.current.style.width)
     }
+
+    const min_box = () => {
+        Diagrampane.current.style.width = '600px'
+        minimize.current.style.display = 'none';
+        expand1.current.style.display = 'flex';
+        console.log(Diagrampane.current.style.width)
+    }
+
     return (
-
-        <div className='expand' onClick={expand_box}>
-            <FaExpand />
+        <div ref={Diagrampane} className='Diagram__pane'>
+            <div ref={expand1} className='expand' onClick={expand_box}>
+                <FiMaximize />
+            </div>
+            <div ref={minimize} className='minimize' onClick={min_box}>
+                <FiMinimize />
+            </div>
+            <div className='diagramBox'></div>
         </div>
-
     )
 }
 
